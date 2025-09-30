26b93e55632612657e8170764e4882ca855be79a
vladutmocanugabriel <mocanuvladutgabriel@gmail.com>
2025-09-30 17:36:00 +0300

    added the recursive page generation function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 from src.helpers import clean_public, copy_to_public
 from src.blocks import markdown_to_html_node, markdown_to_blocks
 
@@ -11,9 +11,10 @@ def extract_title(markdown):
     raise Exception("You're missing the h1 element.")
 
 def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
+    #print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
 
     with open(from_path, "r") as markdown_file:
+        print(f"We're opening and reading the file from: {from_path}")
         markdown_content = markdown_file.read()
 
     with open(template_path, "r") as template_file:
@@ -32,17 +33,39 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as f:
         f.write(template_content)
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for entry in os.listdir(dir_path_content):
+        entry_path = os.path.join(dir_path_content, entry)
+
+        if os.path.isdir(entry_path):
+            dst_subdir = os.path.join(dest_dir_path, entry)
+            os.makedirs(dst_subdir, exist_ok=True)
+            generate_pages_recursive(entry_path, template_path, dst_subdir)
+
+        elif os.path.isfile(entry_path):
+            name, ext = os.path.splitext(entry)
+            if ext.lower() != ".md":
+                continue
+
+            dest_file = os.path.join(dest_dir_path, name + ".html")
+            generate_page(entry_path, template_path, dest_file)
+            
+            
+
+
+    pass
+
 def main():
     root = os.getcwd()
     public_path = os.path.join(root, "public")
     static_path = os.path.join(root, "static")
-    content_path = os.path.join(root, "content/index.md")
+    content_path = os.path.join(root, "content")
     template_path = os.path.join(root, "template.html")
     dest_path = os.path.join(public_path, "index.html")
 
     clean_public(public_path)
     copy_to_public(static_path, public_path)
-    generate_page(content_path, template_path, dest_path)
+    generate_pages_recursive(content_path, template_path, public_path)
 
 if __name__ == "__main__":
     main()
